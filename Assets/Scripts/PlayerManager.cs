ef709a4e171777e2820284cda0d72a89ecc00e30
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-07-31 19:08:56 -0500

    Manage player position when changing to smaller level

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -54,6 +54,11 @@ namespace Com.MyCompany.MyGame {
             else {
                 Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
             }
+
+            #if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
         }
 
         void Update() {
@@ -95,6 +100,20 @@ namespace Com.MyCompany.MyGame {
             // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
             Health -= 0.1f*Time.deltaTime;
         }
+
+        #if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level) {
+            this.CalledOnLevelWasLoaded(level);
+        }
+        #endif
+
+        void CalledOnLevelWasLoaded(int level) {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f)) {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
 #endregion
 
 #region Custom
@@ -114,19 +133,34 @@ namespace Com.MyCompany.MyGame {
 #endregion
 
 #region IPunObservable implementation
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-        if (stream.IsWriting) {
-            // We own this player: send the others our data
-            stream.SendNext(IsFiring);
-            stream.SendNext(Health);
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+            if (stream.IsWriting) {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(Health);
+            }
+            else {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+            }
         }
-        else {
-            // Network player, receive data
-            this.IsFiring = (bool)stream.ReceiveNext();
-            this.Health = (float)stream.ReceiveNext();
-        }
-    }
-
 #endregion
+
+#region Private Methods ?
+        #if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode) {
+            this.CalledOnLevelWasLoaded(scene.buildIndex);
+        }
+        #endif
+#endregion
+
+        #if UNITY_5_4_OR_NEWER
+        public override void OnDisable() {
+            // Always call the base to remove callbacks
+            base.OnDisable ();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        #endif
     }
 }
